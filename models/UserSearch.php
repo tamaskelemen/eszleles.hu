5386ff76ff822e29d072db3d268e3ec645f274e0
kelementamas <tamas.kelemen@antavo.com>
2021-03-30 20:45:15 +0200

    Default order at user index

--- models/UserSearch.php
+++ models/UserSearch.php
@@ -47,6 +47,11 @@ class UserSearch extends User
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort'=> [
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ],
         ]);
 
         $this->load($params);
--- views/user/index.php
+++ views/user/index.php
@@ -20,9 +20,9 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+//            ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+//            'id',
             'email:email',
             'name',
             'last_login',
