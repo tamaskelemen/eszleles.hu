a6eb0748918a539c1b6beb8cd7d54615d0327129
kelementamas <tamas.kelemen@antavo.com>
2021-04-12 23:33:04 +0200

    coloring second comment item

--- views/_common-items/_comment.php
+++ views/_common-items/_comment.php
@@ -1,12 +1,14 @@
 <?php
 /** @var $model \app\models\Comment */
+/** @var $index int
+ */
 
 use yii\helpers\Html;
 use yii\helpers\Url;
 $user = $model->user;
 ?>
 
-<div>
+<div class="p-3 <?= $index % 2 == 0 ? "light-grey-background" : "" ?>">
     <p><?= Html::a($user->name, Url::toRoute(["/user/profile", "id"=> $user->id])) ?> - <?= $model->created_at ?></p>
 
     <p> <?= Html::encode($model->comment) ?></p>
