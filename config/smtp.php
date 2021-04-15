552ca404d2b36e66ddaef8c7675ae64e4559e754
kelementamas <tamas.kelemen@antavo.com>
2021-04-15 21:54:30 +0200

    im a noob and now im removing the test smtp burned into code

--- config/smtp.php
+++ config/smtp.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'Swift_SmtpTransport',
     'encryption' => 'ssl',
-    'host' => 'smtp.gmail.com',
-    'port' => '465',
-    'username' => 'tomizulu@gmail.com',
-    'password' => 'praclin18',
+    'host' => '',
+    'port' => '',
+    'username' => '',
+    'password' => '',
 ];
