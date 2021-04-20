f8df6a057a7a6ba638b02f148d95714ee3954764
kelementamas <tamas.kelemen@antavo.com>
2021-04-20 23:41:18 +0200

    updated news

--- views/site/index.php
+++ views/site/index.php
@@ -22,27 +22,57 @@ $this->title = 'VCSE Észlelések';
     </div>
 
     <div class="body-content container">
-
-        <h2>Hírek</h2>
-        <p><i>2021-04-25</i></p>
-        <div class="row">
-            <div class="col">
-                <h5>Elindult az észlelésfeltöltő!</h5>
-                <p>
-                    A Vega Csillagászati egyesület elindította a saját észlelésfeltöltőjét,
-                    nem csak a tagjainak. Bárki ingyenesen regisztrálhat, és feltöltheti
-                    akár képes, akár szöveges észleléseit, asztrofotóit.
-                </p>
-            </div>
-        </div>
-
         <h2 class="pt-4">Legújabb feltöltések</h2>
+
         <div class="row">
             <?php
             foreach ($latestObs as $obs) {
                 echo $this->render('../_common-items/_latestItem', ['model' => $obs]);
             }
             ?>
+        </div>
+        <hr>
+        <h2 class="pb-4">Hírek</h2>
+        <div class="row mb-5">
+            <div class="col-12">
+                <h5>VEGA észlelések feltöltve</h5>
+                <p><i>2021-04-20</i></p>
+
+                <p>
+                    A <?= Html::a('VEGA', 'http://vcse.hu/vega-lista/') ?> a Vega Csillagászati egyesület folyóirata, amelyben évtizedek óta publikálva vannak az
+                    amatőrcsillagászati megfigyelések. Ezeket összegyűjtve, rendezett formában elérhetővé tettük
+                    weboldalunkon.
+                </p>
+                <p>
+                    A VEGA-ban közel 30 évre visszamenőleg találhatunk amatőrcsillagász megfigyeléseket.
+                    Most végre lehetőség nyílik hosszú idő távlatából újraolvasni
+                    egy-egy adott csillaghalmaz, üstökös, vagy épp nóva megfigyelést.
+                    Érdekes lehet felfedezni, az idő folyamán milyen változások
+                    látszanak - akár a természet változása, akár a távcsövek, eszközök
+                    fejlődése miatt.
+                </p>
+            </div>
+            <hr>
+            <div class="col-12">
+                <h5>Elindult az észlelésfeltöltő!</h5>
+                <p><i>2021-04-19</i></p>
+
+                <p>
+                    A <?= Html::a('Vega Csillagászati Egyesület', 'http://vcse.hu') ?> elindította a saját észlelésfeltöltőjét,
+                    nem csak a tagjainak. Bárki ingyenesen regisztrálhat, és feltöltheti
+                    akár képes, akár szöveges észleléseit, asztrofotóit. Célunk, hogy
+                    hosszútávra és szervezetten, kereshető formában megőrizzük a
+                    magyar amatőrcsillagász társadalom hosszú évek
+                    alatt gyűjtött megfigyeléseit.
+                </p>
+                <p>
+                    Folyamatosan fejlesztjük az oldalt.<br>
+                    Az oldalon szabadidőnkben, bármilyen profit nélkül dolgozunk,
+                    de a terveink listája hosszú. Ahogy időnk engedi, egyre több
+                    funkcióval szeretnénk ellátni oldalunk (amatőr)csillagász társaink
+                    örömére
+                </p>
+            </div>
         </div>
 
     </div>
