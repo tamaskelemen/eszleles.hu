cae5d303142a5cfe6b178e80fa73f7ecddf1ff6c
Florian Vamosi <florian@ultima.space>
2022-02-22 10:25:46 +0100

    Solve migration for now

---
+++ sample/smtp.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'localhost',
+                'username' => 'username',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+        // ...
+    ],
+    // ...
+];
